1fddb55fe33099bbc273c3e3580b665d5a89c370
zkdlu <zkdlu159951@gmail.com>
2020-10-15 17:40:17 +0900

    first commit

---
+++ src/main/java/com/zkdlu/blocking/BlockingApplication.java
@@ -0,0 +1,13 @@
+package com.zkdlu.blocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingApplication.class, args);
+    }
+
+}
